73dfee6e945922fb7878d81e7fadcc8013cfc25c
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-12 13:29:56 -0700

    First attempt at writing the custom query in AnimalRepository

--- src/main/java/local/alfonso/zoo/repos/AnimalRepository.java
+++ src/main/java/local/alfonso/zoo/repos/AnimalRepository.java
@@ -1,13 +1,28 @@
 package local.alfonso.zoo.repos;
 
 import local.alfonso.zoo.model.Animal;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
 
 public interface AnimalRepository extends CrudRepository<Animal, Long>
 {
-//    SELECT
-//    FROM
-//    INNER JOIN
-//    ON
-//    GROUP BY 
+//    SELECT a.zooid, z.zooname, count(a.animalid) as countanimals
+//    FROM zooanimals a
+//    INNER JOIN zoo z
+//    ON a.zooid = z.zooid
+//    GROUP BY a.zooid, z.zooname
+
+    @Query(value = "SELECT a.zooid, z.zooname, count(a.animalid) as countanimals FROM zooanimals a NNER JOIN zoo z ON a.zooid = z.zooid GROUP BY a.zooid, z.zooname")
+    ArrayList<?> getCountAnimalsInZoo();
+
+//    DELETE
+//    FROM zooanimals
+//    WHERE zooid =
+
+    @Modifying
+    @Query(value = "DELETE FROM zooanimals WHERE zooid = :zooid", nativeQuery = true)
+    void deleteZooFromZooanimals(long zooid);
 }
